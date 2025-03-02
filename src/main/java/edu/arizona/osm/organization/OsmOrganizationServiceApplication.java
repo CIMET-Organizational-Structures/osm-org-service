feac4cff41061dcf584c07357260857f2ddb17cc
Jose Sosa <josesosa@arizona.edu>
2025-03-02 00:37:56 -0500

    Adopted Spring Java Formatter.

--- src/main/java/edu/arizona/osm/organization/OsmOrganizationServiceApplication.java
+++ src/main/java/edu/arizona/osm/organization/OsmOrganizationServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OsmOrganizationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OsmOrganizationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OsmOrganizationServiceApplication.class, args);
+    }
 
 }
--- src/test/java/edu/arizona/osm/organization/OsmOrganizationServiceApplicationTests.java
+++ src/test/java/edu/arizona/osm/organization/OsmOrganizationServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OsmOrganizationServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
