7fee5ff8adc4304073fb27c8871bdbf65fdd35ec
Jose Sosa <josesosa@arizona.edu>
2025-03-02 00:31:12 -0500

    Initial commit

---
+++ src/main/java/edu/arizona/osm/organization/OsmOrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.arizona.osm.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmOrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmOrganizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/arizona/osm/organization/OsmOrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.arizona.osm.organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmOrganizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
